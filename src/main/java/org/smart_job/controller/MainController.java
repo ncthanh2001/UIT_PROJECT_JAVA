04fcea23f9efe6bb77b92247d5ef28ebbd9f052f
OssiLV <vohoanganhtruong@gmail.com>
2025-10-01 23:13:10 +0700

    fix(job): show list jobs

--- src/main/java/org/smart_job/controller/MainController.java
+++ src/main/java/org/smart_job/controller/MainController.java
@@ -10,6 +10,7 @@ import org.smart_job.view.auth.LoginView;
 import org.smart_job.view.auth.RegisterView;
 import org.smart_job.view.dashboard.DashboardContentPanel;
 import org.smart_job.view.jobs.JobTrackerContentPanel;
+import org.smart_job.view.jobs.ListJobsContentPannel;
 import org.smart_job.view.profile.ProfileContentPanel;
 import org.smart_job.service.impl.UserServiceImpl;
 
@@ -40,6 +41,7 @@ public class MainController {
     private void registerEvents() {
         view.getDashboardButton().addActionListener(e -> showDashboard());
         view.getCvAnalysisButton().addActionListener(e -> showCVAnalysis());
+        view.getJobsButton().addActionListener(e -> showListJobs());
         view.getJobTrackerButton().addActionListener(e -> showJobTracker());
         view.getProfileButton().addActionListener(e -> showProfile());
         view.getLogoutButton().addActionListener(e -> {
@@ -55,10 +57,19 @@ public class MainController {
         DashboardController dashboardController = new DashboardController(panel);
 
         // Set up navigation handlers for dashboard buttons
-        dashboardController.setNavigateToJobTracker(() -> showJobTracker());
-        dashboardController.setNavigateToCVAnalysis(() -> showCVAnalysis());
+        dashboardController.setNavigateToJobTracker(this::showJobTracker);
+        dashboardController.setNavigateToCVAnalysis(this::showCVAnalysis);
 
         view.setContent(panel);
+    }
+
+    public void showListJobs() {
+        view.setVisible(true);
+        ListJobsContentPannel panel = new ListJobsContentPannel();
+        new ListJobsController(panel);
+
+        view.setContent(panel);
+
     }
 
     public void showCVAnalysis() {
